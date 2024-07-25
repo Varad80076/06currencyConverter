8936fd8042bfa6e315b587a1b08d4d7c94b688e4
Varad80076 <badgujarvarad800@gmail.com>
2024-07-25 08:28:32 +0530

    bg-size

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
     return (
       <div
           className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat "
-         style={{ backgroundImage: `url('https://images.unsplash.com/photo-1502920514313-52581002a659?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,}}
+        
       >
           <div className="w-full">
               <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
